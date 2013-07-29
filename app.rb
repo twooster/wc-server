13bdbe2845aaea3be5b744fc601ee1f90b0a796e
Tony Wooster <twooster@gmail.com>
2013-07-28 22:35:59 -0700

    Add basic admin, flash infrastructure

--- app.rb
+++ app.rb
@@ -6,6 +6,54 @@ require 'haml'
 require './config'
 require './models'
 
+enable :sessions
+set :session_secret, 'seekret seekret'
+
+def flash(type, msg)
+  session['flash'] ||= {}
+  session['flash'][type] ||= []
+  session['flash'][type] << msg
+
+  @flash[type] ||= []
+  @flash[type] << msg
+end
+
+before do
+  @flash = session['flash'] || {}
+  session.delete 'flash'
+end
+
 get '/' do
   haml :index
+end
+
+get '/login' do
+  haml :'admin/login'
+end
+
+post '/login' do
+  unless params[:password] == 'seekret'
+    flash :error, 'Permission denied'
+    haml :'admin/login'
+  else
+    session['admin'] = true
+    redirect to('/admin')
+  end
+end
+
+get '/logout' do
+  session.delete 'admin'
+  session.delete 'uid'
+  redirect to('/')
+end
+
+before %r{/admin(/.*)?} do
+  unless session['admin'] == true
+    flash :error, 'Permission denied'
+    redirect to('/login')
+  end
+end
+
+get '/admin' do
+  haml :'admin/index'
 end
