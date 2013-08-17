444838a826846a4dbaae2cec8be9a194b46ffe61
Tony Wooster <twooster@gmail.com>
2013-08-17 00:33:54 -0700

    Update cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,15 +6,23 @@ set :deploy_to,   '/home/whatcropaccount/v2.whatcrop.org'
 set :scm, :git
 set :use_sudo, false
 set :git_enable_submodules, 1
+set :rake, 'bundle exec rake'
+
+set :default_shell, "sh -l"
 
 server 'v2.whatcrop.org', :web, :app, :db, :primary => true
 
 after 'deploy:restart', 'deploy:cleanup'
+after 'deploy:finalize_update', 'deploy:bundle_update'
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :bundle_update, :roles => :app do
+    run "cd #{current_path} && bundle check || bundle install"
   end
 end
